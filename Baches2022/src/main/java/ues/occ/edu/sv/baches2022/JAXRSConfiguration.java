b7b8c0100d229a932f6db3e3e39b24d6233a3c25
bryan <bryan@LAPTOP-G0815DU8>
2022-03-24 16:28:55 -0600

    Mapeo BD

---
+++ Baches2022/src/main/java/ues/occ/edu/sv/baches2022/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package ues.occ.edu.sv.baches2022;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
